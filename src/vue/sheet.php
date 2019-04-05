3d31ccdfbbf8003201a20d05cc5620e535842c3b
cerise <cerise.andres@yahoo.fr>
2019-04-05 16:20:39 +0200

    dhdhgchcgh

--- src/vue/sheet.php
+++ src/vue/sheet.php
@@ -15,6 +15,7 @@
 
     var name='<?php echo $this->var[0]->data['username']?>';
     var password='<?php echo $this->var[0]->data['user_password']?>';
+    var password='<?php echo $this->var[0]->data['api']?>';
 
 </script>
 
