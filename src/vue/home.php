5af30ee99ab38240c58679fe59bb232fe0e908bf
Elineda <henecro@yahoo.fr>
2019-06-06 10:32:19 +0200

    Merge branch 'master' of https://github.com/elineda/aionGRP
    # Conflicts:
    #	js/map.js
    #	package-lock.json
    #	package.json
    #	src/controller/personnage.php
    #	src/model/Blog.php
    #	src/model/Character.php
    #	src/vue/home.php
    #	src/vue/map.php
    #	src/vue/page.php
    #	src/vue/sheet.php
    #	style.css

--- src/vue/home.php
+++ src/vue/home.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 <h3> What GRP is it ?</h3>
 
 
@@ -22,28 +20,3 @@ Free to download our game, (different than eu or na official) and take on look o
                 Without new daevas the full scale war that both the Elyos and Asmodae waged against eachother has degraded into smaller scale scuffles now that immortality is an asset lost to time.
 
             </p>
-=======
-
-<h3> What GRP is it ?</h3>
-
-
-<p>We are a roleplay server in Aion. What's mean this ? <br>
-It's mean we don't do pve or pvp in this server. All instance are down and all players are in a neutral faction.<br>
-You wan enjoy without barrier all role play features in aion : Housing, Infinite character appearance in aion, a lot of skin for free !<br>
-Free to download our game, (different than eu or na official) and take on look on discord and forum for further information !</p>
-
-            <h3> Quick lore : </h3>
-            <p>This server takes place in  the Post-Dream Era which has started after the planet has stabilized itself from the bleeding aether that once threatened to split it further than it already has.
-                During this bleeding period the overabundance of aether has caused various instabilities most notably disproportionate sizes and dimensional rifts beyond what was previously experienced.
-                This explains how gargantuan beings came to be and why foreigners started popping up in Atreia</p>
-            <p>
-                When the planet had fixed itself, it entered a state of rest which significantly throttled the flow of aether evidenced by the warm core of the abyss had turning cold.
-                As a result, aether had become far less available and usable to the point where the societies had abandoned it in search of alternative methods.
-                Many existing infrastructures had to be rebuilt from the ground up to implement these new methods and as a result society has visually not changed in the past few centuries.
-                The lack of aether has also lead to the stagnation and slow decline of daevas as an ascension hasn't happened since the world slept and Aion's too busy enjoying his retirement.
-            </p>
-            <p>
-                Without new daevas the full scale war that both the Elyos and Asmodae waged against eachother has degraded into smaller scale scuffles now that immortality is an asset lost to time.
-
-            </p>
->>>>>>> 7c85792392bd355cb1b25b71e43b81291d970142
--- src/vue/map.php
+++ src/vue/map.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <h3>Online player</h3>
 <?php $tab=$this->var[0];
 
@@ -53,59 +52,3 @@
 <!--[{"name":"Ellice","0":"Ellice","world_id":"Sanctum","1":"110010000","gender":"FEMALE","2":"FEMALE","race":"ELYOS","3":"ELYOS","player_class":"TEMPLAR","4":"TEMPLAR"},{"name":"Ellineda","0":"Ellineda","world_id":"Poeta","1":"600100000","gender":"FEMALE","2":"FEMALE","race":"ASMODIANS","3":"ASMODIANS","player_class":"SORCERER","4":"SORCERER"}]-->
 
 <script src="/aionGRP/js/map.js"></script>
-=======
-<h3>Online player</h3>
-<?php $tab=$this->var[0];
-
-
-
-?>
-
-    <link rel="stylesheet" href="/aionGRP/map.css">
-    <script
-    			  src="https://code.jquery.com/jquery-3.3.1.js"
-    			  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-    			  crossorigin="anonymous"></script>
-
-    <nav style="display: inline-flex; justify-content: space-around;margin: 0 auto; width: 100%;">
-      <button type="button" id="elyseab" class="btn btn-map">Elysea</button>
-      <button type="button" id="asmodeeb" class="btn btn-map">Asmodee</button>
-      <button type="button" id="balaureab" class="btn btn-map">Balaurea</button>
-    </nav>
-    <div id="elysea">
-      <div id="Sanctum" class="map" style=" position: absolute; height: 30px; width: 30px;  top: 140px; left: 295px;"></div>
-      <div id="Poeta" class="map" style=" position: absolute; height: 30px; width: 30px; top: 240px; left: 150px;"></div>
-      <div id="Oriel" class="map" style=" position: absolute; height: 30px; width: 30px; top: 210px; left: 280px;"></div>
-      <div id="Iluma" class="map" style=" position: absolute; height: 30px; width: 30px; top: 140px; left: 450px;"></div>
-      <div id="Verteron" class="map" style=" position: absolute; height: 30px; width: 30px; top: 280px; left: 290px;"></div>
-      <div id="Eltnen" class="map" style=" position: absolute; height: 30px; width: 30px; top: 380px; left: 200px;"></div>
-      <div id="Heiron" class="map" style=" position: absolute; height: 30px; width: 30px; top: 400px; left: 370px;"></div>
-      <div id="Theobomos" class="map" style=" position: absolute; height: 30px; width: 30px; top: 270px; left: 545px;"></div>
-    </div>
-
-    <div id="asmodee">
-      <div id="Brusthonin" class="map" style=" position: absolute; height: 30px; width: 30px; top: 300px; left: 100px;"></div>
-      <div id="Morheim" class="map" style=" position: absolute; height: 30px; width: 30px; top: 230px; left: 230px;"></div>
-      <div id="Beluslan" class="map" style=" position: absolute; height: 30px; width: 30px; top: 210px; left: 380px;"></div>
-      <div id="Pandaemonium" class="map" style=" position: absolute; height: 30px; width: 30px; top: 320px; left: 230px;"></div>
-      <div id="Pernon" class="map" style=" position: absolute; height: 30px; width: 30px; top: 380px; left: 260px;"></div>
-      <div id="Altgard" class="map" style=" position: absolute; height: 30px; width: 30px; top: 320px; left: 420px;"></div>
-      <div id="Ishalgen" class="map" style=" position: absolute; height: 30px; width: 30px; top: 420px; left: 340px;"></div>
-      <div id="Norsvold" class="map" style=" position: absolute; height: 30px; width: 30px; top: 460px; left: 190px;"></div>
-    </div>
-
-    <div id="balaurea">
-      <div id="Inggison" class="map" style=" position: absolute; height: 30px; width: 30px; top: 190px; left: 160px;"></div>
-      <div id="SilenteraCanyon" class="map" style=" position: absolute; height: 30px; width: 30px; top: 230px; left: 250px;"></div>
-      <div id="Gelkmaros" class="map" style=" position: absolute; height: 30px; width: 30px; top: 140px; left: 410px;"></div>
-      <div id="Kaldor" class="map" style=" position: absolute; height: 30px; width: 30px; top: 370px; left: 120px;"></div>
-      <div id="Signia" class="map" style=" position: absolute; height: 30px; width: 30px; top: 330px; left: 220px;"></div>
-      <div id="Vengar" class="map" style=" position: absolute; height: 30px; width: 30px; top: 330px; left: 300px;"></div>
-      <div id="Lakrum" class="map" style=" position: absolute; height: 30px; width: 30px; top: 350px; left: 430px;"></div>
-    </div>
-
-
-<!--[{"name":"Ellice","0":"Ellice","world_id":"Sanctum","1":"110010000","gender":"FEMALE","2":"FEMALE","race":"ELYOS","3":"ELYOS","player_class":"TEMPLAR","4":"TEMPLAR"},{"name":"Ellineda","0":"Ellineda","world_id":"Poeta","1":"600100000","gender":"FEMALE","2":"FEMALE","race":"ASMODIANS","3":"ASMODIANS","player_class":"SORCERER","4":"SORCERER"}]-->
-
-<script src="/aionGRP/js/map.js"></script>
->>>>>>> 7c85792392bd355cb1b25b71e43b81291d970142
